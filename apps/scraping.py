e62913827f310bc06df1abd27368600732c7c1df
spencerkopp <spencertkopp@gmail.com>
2022-09-04 19:47:30 -0400

    Updating scraping.py and m2mchallenge.py.

--- apps/scraping.py
+++ apps/scraping.py
@@ -5,23 +5,25 @@ import pandas as pd
 import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
-
     news_title, news_paragraph = mars_news(browser)
-
+    # hemisphere_image_urls = hemi_scrape(browser)
+    hemisphere_image_urls = hemi_scrape(browser)
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_image_urls": hemisphere_image_urls
+        # "hemi_url" = hemi_url
+        # "title" = title
+        
     }
-
     # Stop webdriver and return data
     browser.quit()
     return data
@@ -97,7 +99,24 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-if __name__ == "__main__":
+def hemi_scrape(browser): 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    hemisphere_image_urls = []
+    for i in range(4):
+        hemi = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+        img_url = img_soup.find('img', class_='wide-image').get('src')
+        full_url = f'https://marshemispheres.com/{img_url}'
+        title = browser.find_by_css("h2.title").text
+        hemi['image_url'] = full_url
+        hemi['title'] = title
+        hemisphere_image_urls.append(hemi)
+        browser.back()
+    return hemisphere_image_urls
 
+if __name__ == "__main__": 
     # If running as script, print scraped data
     print(scrape_all())
